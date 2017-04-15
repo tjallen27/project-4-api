15d9a3fd99a164c5737193c32d04b6975626ed78
tjallen27 <tjallen91@hotmail.co.uk>
2017-04-15 16:03:21 +0100

    Change image source

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,18 @@ tom = User.create!(
 
 Print.create!(
 title: "Ocean",
-image: "https://postimg.org/image/tcp6uj3g1/",
+image: "https://s24.postimg.org/r84ttg1th/ocean.jpg",
 height: 12,
+width: 24,
+medium: "Print",
+price: 20,
+body: "Blurb about the print",
+user: tom
+)
+Print.create!(
+title: "Ram",
+image: "https://s14.postimg.org/s2641mlxd/IMG_3238.jpg",
+height: 40,
 width: 24,
 medium: "Print",
 price: 50,
