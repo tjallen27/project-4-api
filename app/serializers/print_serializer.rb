be03cbd0eb89f2a33dc0ffd41330164692f89fe7
tjallen27 <tjallen91@hotmail.co.uk>
2017-04-20 12:59:25 +0100

    Comments done

--- app/serializers/print_serializer.rb
+++ app/serializers/print_serializer.rb
@@ -1,5 +1,5 @@
 class PrintSerializer < ActiveModel::Serializer
-  attributes :id, :title, :image_src, :height, :width, :medium, :price, :body
+  attributes :id, :title, :image_src, :height, :width, :medium, :price, :body, :user
   belongs_to :user
   has_many :comments
 
