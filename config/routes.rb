e022c200b54ad203c840e3736e105786cc06333e
tjallen27 <tjallen91@hotmail.co.uk>
2017-04-24 10:51:24 +0100

    Removing index controller :fire:

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-
   resources :orders
   resources :comments
-  root :to => 'index#index'
   resources :prints
   resources :users
   post 'register', to: 'authentications#register'
